fceb2e0246cbb3c47843a7f5832095d5d155db11
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-05-12 14:14:51 +0200

    ajout de dose possible

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,25 @@
 class DosesController < ApplicationController
-  def new
-  end
 
   def create
+    @ingredient = Ingredient.find(params[:dose][:ingredient])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
 end
