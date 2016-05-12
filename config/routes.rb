dd35e2ba4d523ad73e5e58e8fe352e5d80a9917d
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-05-12 11:51:15 +0200

    avec root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'cocktails'
+
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
