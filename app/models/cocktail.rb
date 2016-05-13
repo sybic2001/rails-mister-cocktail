9220c1e786cc7669f171e8628c5055f60b7269f6
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-05-13 11:49:51 +0200

    ajout d'image fonctionnel

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ActiveRecord::Base
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_attachment :photo
   validates :name, presence: true
   validates :name, uniqueness: true
 
